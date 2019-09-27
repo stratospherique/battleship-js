48152c9d11ee6781a4ffa85f902d14e9b4e17763
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-09-27 21:51:10 +0100

    add a grid to gameboard

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -1,7 +1,24 @@
-const GameBoard = ship => ({
-  grid: new Array(100),
-  ship,
+const gridT = [
+  'ABCDEFGHIJ',
+  '0123456789',
+]
+
+const grid = () => {
+  const tmp = new Array(10);
+  gridT[0].split('').forEach((item, i) => {
+    tmp[i] = [];
+    gridT[1].split('').forEach((suItem) => {
+      tmp[i].push(`${item}${suItem}`);
+    });
+  });
+  return tmp;
+}
+
+const GameBoard = () => ({
+  grid: grid(),
   positionShip(ship, position, facing) {
-    const validPosition = position 
+    const validPosition = position
   }
 })
+
+export default GameBoard;
---
+++ test/gameboard.test.js
@@ -0,0 +1,6 @@
+import GameBoard from '../src/gameBoard';
+
+it.only('test a gameboard', () => {
+  const board = GameBoard();
+  console.log(board.grid);
+})
