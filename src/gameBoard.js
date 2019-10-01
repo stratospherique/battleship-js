b51c8a86be595bc5e0f8414ea00fea5ea5f84816
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-10-01 15:47:43 +0100

    worked on the gameboard factory function

--- src/gameBoard.js
+++ src/gameBoard.js
@@ -3,39 +3,46 @@ const gridT = [
   '0123456789',
 ]
 
-const grid = () => {
-  const tmp = new Array(10);
-  gridT[0].split('').forEach((item, i) => {
-    tmp[i] = [];
-    gridT[1].split('').forEach((suItem) => {
-      tmp[i].push(`${item}${suItem}`);
+const grid = new Array(10);
+gridT[0].split('').forEach((item, i) => {
+  tmp[i] = [];
+  gridT[1].split('').forEach((suItem) => {
+    grid[i].push({
+      pos: `${item}${suItem}`,
+      state: 'empty',
     });
   });
-  return tmp;
-}
+});
 
 const GameBoard = () => ({
-  grid: grid(),
-  validPosition: (ship, coord, facing) => {
-    let valid = null;
-    const cordNum = parseInt(coord.split('')[1]);
+  board: grid,
+  validPosition: (ship, coord, facing, board) => {
+    let valid = true;
+    const cordNum = parseInt(coord.pos.split('')[1]);
+    const cordV = gridT[0].indexOf(coord.pos.split('')[0]);
     if (facing === 'horizontal') {
-      switch (ship.length) {
-        default:
-          valid = true;
-          break;
-        case ship.length === 2 && cordNum === 9:
-          valid = false;
-          break;
-        case ship.length === 3 && cordNum > 7:
-          valid = false;
-          break;
-        case ship.length === 4 && cordNum > 6:
-          valid = false;
-          break;
+      for (let j = cordNum; j <= (cordNum + ship.length); j++) {
+        if (board[cordV][j].state !== 'empty') {
+          return false;
+        }
+      }
+    } else {
+      for (let i = cordV; i <= (cordV + ship.length); i++) {
+        if (board[i][cordNum].state !== 'empty') {
+          return false;
+        }
       }
     }
-    return valid;
+    return true;
+  },
+  placeShip: (ship, board) => {
+    const [randomH, randomV] = 0;
+    do {
+      randomH = Math.round(Math.random() * 9);
+      randomV = Math.round(Math.random() * 9);
+      let coord = board[randomV][randomH];
+      let facing = ['horizontal', 'vertical']
+    } while (validPosition(ship, coord, facing, board));
   }
 })
 
