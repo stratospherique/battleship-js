1b7a11602bbac0ee65ca7040ec947e9622ed3bea
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-10-02 18:08:41 +0100

    add a solution to seperate board objects for each test

--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -5,22 +5,23 @@ describe('testing the gameboard', () => {
   it('creates a 10X10 grid', () => {
     const ships = [Ship(1, ["A1"])]
     const game = GameBoard(ships);
-    expect(game.board.length).toBe(10);
     expect(game.board[0].length).toBe(10);
+    expect(game.board.length).toBe(10);
   });
 
   it('places a battelship on the grid', () => {
-    const ships = [Ship(2)];
+    const ships = [Ship(6)];
     const game = GameBoard(ships);
     game.placeShip();
     const [x, y] = [parseInt(ships[0].position[0][0]), parseInt(ships[0].position[0][1])];
-    expect(game.board[x][y].state).toBe('taken');
+    //expect(game.board[x][y].state).toBe('taken');
+    expect(game.board).toBe('taken');
   });
 
   it('places a array of ships on the grid', () => {
     const shipsArray = [Ship(4), Ship(3), Ship(3), Ship(3)];
-    const game = GameBoard(shipsArray);
+    const game = Object.assign({}, GameBoard(shipsArray));
     game.placeShip();
     expect(game.board).toBe(0);
-  })
+  });
 });
