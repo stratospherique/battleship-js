aab174cf70e57e4c998440be94045a3ec3ac2feb
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-10-16 17:28:59 +0100

    Fill html content dynamically board

--- src/DOMcontent.js
+++ src/DOMcontent.js
@@ -1,6 +1,24 @@
-import GameBoard from './gameBoard';
-
-const playerBoard = document.querySelector('.player__board');
-
+const playerBoard = (() => {
+  const playerBoard = document.querySelector('.player__board');
+  //const botBoard = document.querySelector('.bot__board');
+  const buildBoard = (board) => {
+    const html = board.map((line) => {
+      const row = line.map((cell, ind) => {
+        return
+        `
+        <div class="col" id="${ind}">${ind}</div>
+        `
+      }).join('');
+      return
+      `
+      <div class="row">${row}</div>
+      `
+    }).join('');
+    playerBoard.innerHTML = html;
+  }
+  return {
+    buildBoard
+  }
+})();
 export default playerBoard;
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import _ from 'lodash';
 import './assets/css/main.scss';
 import playerBoard from './DOMcontent';
+import GameBoard from './gameBoard';
+import Ship from './ships';
 
+const game = GameBoard([Ship(1)]);
+playerBoard.buildBoard(game.board);
 
