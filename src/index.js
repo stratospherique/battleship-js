a9ffc94851bff983bf626261ee1cf8942a8f0cb7
Jorge <jofer86@gmail.com>
2019-10-22 16:17:40 -0500

    add style, comp win message and reset button.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-import './assets/css/main.scss';
-import computerBoard from './DOMcontent';
-import GameBoard from './gameBoard';
-import Ship from './ships';
-import Player from './player';
+import "./assets/css/main.scss";
+import computerBoard from "./DOMcontent";
+import GameBoard from "./gameBoard";
+import Ship from "./ships";
+import Player from "./player";
 
 let compGame = null;
 let playerGame = null;
-const compPlayer = Player('Computer', 'bot');
-const player = Player('Clarence', 'Human');
+const compPlayer = Player("Computer", "bot");
+const player = Player("Clarence", "Human");
 let turn;
 let plHits;
 let cpHits;
 // Game loop
-
 
 const gameStart = () => {
   compPlayer.initMoves();
@@ -28,7 +27,7 @@ const gameStart = () => {
   plHits = 0;
   cpHits = 0;
   computerBoard.updateBanner(true, 0);
-  computerBoard.updateBanner(false, 0)
+  computerBoard.updateBanner(false, 0);
 };
 
 gameStart();
@@ -36,31 +35,39 @@ gameStart();
 // Computer move
 const compMove = () => {
   if (!turn) {
-    // console.log(playerGame.board);
     const arr = compPlayer.play();
     const [crd1, crd2] = arr;
-    arr.unshift('P');
+    arr.unshift("P");
     cpHits += 1;
     computerBoard.updateBanner(turn, cpHits);
-    const div = document.getElementById(arr.join(''));
+    const div = document.getElementById(arr.join(""));
     playerGame.recieveAttack(crd1, crd2);
     computerBoard.changeCell(div, playerGame.board[crd1][crd2]);
-    setTimeout(() => { turn = !turn; }, 1000);
+    setTimeout(() => {
+      turn = !turn;
+    }, 1000);
     if (playerGame.gameOver()) {
-      computerBoard.gameMessage(false);
-      gameStart();
+      computerBoard.wrapper.innerHTML = computerBoard.computerWin;
+      document.querySelector(".gameStart").addEventListener("click", refresh);
+      return;
     }
   }
 };
 
-window.onclick = (e) => {
+const refresh = () => {
+  window.location.reload(false);
+};
+
+window.onclick = e => {
   let cord1 = 0;
   let cord2 = 1;
   if (turn) {
-    if (e.target.classList.value.includes('bot')) {
-      cord1 = parseInt(e.target.id.split('')[0]);
-      cord2 = parseInt(e.target.id.split('')[1]);
-    } else { return; }
+    if (e.target.classList.value.includes("bot")) {
+      cord1 = parseInt(e.target.id.split("")[0]);
+      cord2 = parseInt(e.target.id.split("")[1]);
+    } else {
+      return;
+    }
     if (player.play(compGame.board[cord1][cord2])) {
       compGame.recieveAttack(cord1, cord2);
       computerBoard.changeCell(e.target, compGame.board[cord1][cord2]);
@@ -68,13 +75,13 @@ window.onclick = (e) => {
       computerBoard.updateBanner(turn, plHits);
       turn = !turn;
       if (compGame.gameOver()) {
-        computerBoard.gameMessage(true);
-        gameStart();
+        computerBoard.wrapper.innerHTML = computerBoard.playerWin;
+        document.querySelector(".gameStart").addEventListener("click", refresh);
         return;
       }
-      setTimeout(compMove, 2000);
+      setTimeout(compMove, 1500);
     } else {
-      alert('wrong move');
+      alert("wrong move");
     }
   }
 };
